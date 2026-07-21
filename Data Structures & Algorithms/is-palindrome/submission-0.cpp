c7de8e2a1f92dcd07d204d41ef38e5ac67a3b492
Prasad Yeole <yeoleprasad20@gmail.com>
2026-07-21 18:25:17 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string clean = "";
+
+        for(char ch : s) {
+
+            if(isalnum(ch)) {
+                clean += tolower(ch);
+            }
+        }
+
+        string rev = clean;
+
+        reverse(rev.begin(), rev.end());
+
+        return clean == rev;
+    }
+};
